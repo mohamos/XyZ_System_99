a6994333c056b8b825524df925f0ebbfcab1fbe7
MostafaYahia <55910559+mostafayahia2020@users.noreply.github.com>
2026-03-27 21:21:50 +0200

    # Txt Mesh pro + Card Prefab + Card Script
    # First work commit , contains , Importing tmpro Library and Card Prefab that I am going to use Plus First Card Behaviour script that will be responsible about the behaviour of the card

---
+++ AGAPI Task/Assets/Round One Task/Project Systems/Card System/CardBehaviour.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace RoundOne
+{
+    public class CardBehaviour : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private GameObject backLayer;
+        [SerializeField] private Image frontLayer;
+
+        public int CardID { get; private set; }
+        public bool IsFlipped { get; private set; }
+
+      
+        public static event Action<CardBehaviour> OnCardClicked;
+
+        public void Initialize(int id, Color color)
+        {
+            CardID = id;
+            frontLayer.color = color;
+            ShowBack();
+        }
+
+        public void OnClick() 
+        {
+            if (IsFlipped) return; 
+
+            OnCardClicked?.Invoke(this);
+        }
+
+        public void Flip(bool showFront)
+        {
+            IsFlipped = showFront;
+            backLayer.SetActive(!showFront);
+        
+        }
+
+        private void ShowBack()
+        {
+            IsFlipped = false;
+            backLayer.SetActive(true);
+        }
+    }
+}
